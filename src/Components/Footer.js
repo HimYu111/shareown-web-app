44cc4825af4ee5944187e50216427bd3935f37d0
Charlie Yu <hshunyu274@gmail.com>
2024-03-02 03:57:43 +0000

    footer update

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,21 +6,16 @@ function Footer() {
       <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
         <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
           <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-            UCL © 2023{" "}
+            UCL © 2024{" "}
             <a href="https://flowbite.com/" className="hover:underline">
-              Placeholder™
+              University College London, Bartlett School of Sustainable Construction
             </a>
             . All Rights Reserved.
           </span>
           <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
             <li>
-              <a href="#" className="mr-4 hover:underline md:mr-6 ">
-                +44 (0)20 3108 3220
-              </a>
-            </li>
-            <li>
               <a href="#" className="mr-4 hover:underline md:mr-6">
-                bssc.enquiries@ucl.ac.uk{" "}
+                s.milcheva@ucl.ac.uk{" "}
               </a>
             </li>
             <li>
