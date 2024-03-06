d703d2d5c2f8a75cf7c86dff826589dcf2eb3d57
Charlie Yu <hshunyu274@gmail.com>
2024-03-06 06:49:00 +0000

    new output

--- src/Components/Results.js
+++ src/Components/Results.js
@@ -2,41 +2,91 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function Results({ result }) {
-  // Check if result is an object and has the required properties
   const hasResults =
     result &&
     typeof result === "object" &&
-    "affordability_status" in result &&
-    "accumulated_wealth_at_67" in result;
+    "TO_age" in result &&
+    "TO_time" in result &&
+    "TO_finish" in result &&
+    "TO_liquid" in result &&
+    "TO_housing" in result &&
+    "SO_start_age" in result &&
+    "SO_time" in result &&
+    "SO_staircase_finish" in result &&
+    "SO_mortgage_finish" in result &&
+    "SO_liquid" in result &&
+    "SO_housing" in result &&
+    "age_at_time_data" in result;
+
   const hasError = result && typeof result === "object" && "error" in result;
 
   return (
     <div className="bg-slate-800 py-20">
-      <div className="text-white text-4xl" id="results">
+      <div className="text-white" id="results">
         {hasResults ? (
-          <div>
-            <p className="text-3xl">Buying a home on the open market is currently {result.affordability_status} with your current assets</p>
-            <p className="text-2xl">
-              Accumulated wealth at retirement is estimated to be £
-              {result.accumulated_wealth_at_67.toFixed(0)}
-            </p>
-          </div>
+          <>
+            <p className="text-3xl mb-4">Results:</p>
+            <table className="table-auto w-full">
+              <tbody>
+                <tr>
+                  <td>You can afford to buy by the age of: </td>
+                  <td>{result.TO_age}</td>
+                </tr>
+                <tr>
+                  <td>You can afford to buy at the age of: </td>
+                  <td>{result.TO_time}</td>
+                </tr>
+                <tr>
+                  <td>You will be able to repay your mortgage by the age of </td>
+                  <td>{result.TO_finish}</td>
+                </tr>
+                <tr>
+                  <td>By retirement age (67), your liquid wealth would be: </td>
+                  <td>{result.TO_liquid}</td>
+                </tr>
+                <tr>
+                  <td>By retirement age (67), your housing wealth would be: </td>
+                  <td>{result.TO_housing}</td>
+                </tr>
+                <tr>
+                  <td>You can afford to buy into shared ownership by the age of: </td>
+                  <td>{result.SO_start_age}</td>
+                </tr>
+                <tr>
+                  <td>You can afford to buy your first share in a shared ownership property at the age of: </td>
+                  <td>{result.SO_time}</td>
+                </tr>
+                <tr>
+                  <td>You will be able to staircase to full ownership by the age of: </td>
+                  <td>{result.SO_staircase_finish}</td>
+                </tr>
+                <tr>
+                  <td>You will be able to repay your mortgage by the age of: </td>
+                  <td>{result.SO_mortgage_finish}</td>
+                </tr>
+                <tr>
+                  <td>By retirement age (67), your liquid wealth would be: </td>
+                  <td>{result.SO_liquid}</td>
+                </tr>
+                <tr>
+                  <td>By retirement age (67), your housing wealth would be: </td>
+                  <td>{result.SO_housing}</td>
+                </tr>
+              </tbody>
+            </table>
+          </>
         ) : hasError ? (
-          <p className="text-md">Error: {result.error}</p>
+          <p>Error: {result.error}</p>
         ) : (
-          <p className="text-sm">No results to display</p>
+          <p>No results to display</p>
         )}
       </div>
     </div>
-
   );
 }
 
 Results.propTypes = {
-  result: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.object, // result can be a string or an object
-  ]),
+  result: PropTypes.object,
 };
 
 export default Results;
