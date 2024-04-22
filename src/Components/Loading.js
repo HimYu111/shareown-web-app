ade0ee81fb4863f1e8657fafbc1fb77e18f11211
Charlie Yu <hshunyu274@gmail.com>
2024-04-22 03:19:33 +0100

    fix loading

--- src/Components/Loading.js
+++ src/Components/Loading.js
@@ -2,13 +2,11 @@ import React from "react";
 
 function Loading() {
   return (
-    <div className="bg-gray-200 flex justify-center items-center h-screen border-x-[16px] border-white">
-      <div className="text-center ">
-        <h1 id="loading" className="text-4xl">
-          Your results will be ready in a moment
-        </h1>
-        <span className="loading loading-spinner loading-lg"></span>
-      </div>
+    <div className="loading-container">
+      <h1 className="text-3xl text-gray-800">
+        Your results will be ready in a moment
+      </h1>
+      <span className="loading loading-spinner loading-lg"></span>
     </div>
   );
 }
