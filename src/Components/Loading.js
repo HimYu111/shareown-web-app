b86f815774d5686249ac47994b958d77b5571544
unknown <nvasilev@novacore.bg>
2024-05-09 17:33:43 +0300

    NV fixes 2

--- src/Components/Loading.js
+++ src/Components/Loading.js
@@ -3,7 +3,7 @@ import React from "react";
 function Loading() {
   return (
     <div className="loading-container">
-      <h1 className="text-3xl text-gray-800">
+      <h1 className="loading-text">
         Your results will be ready in a moment
       </h1>
       <span className="loading loading-spinner loading-lg"></span>
